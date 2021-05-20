039f193733ee9e067e64d7f93dd5afc08af4d7e7
aw632 <aw632@cornell.edu>
2021-05-20 18:00:35 -0400

    some notty stuff

--- main.ml
+++ main.ml
@@ -5,6 +5,31 @@ open Notty_unix
 open EuclideanAlg
 open StatOp
 open Help
+open Notty
+open Notty_unix
+
+let uchars_maker arr =
+  I.uchars A.(fg lightred) (Array.map Uchar.of_int arr)
+
+let time_converter utim =
+  let record = utim |> Unix.localtime in
+  string_of_int record.tm_hour
+  ^ ":"
+  ^ string_of_int record.tm_min
+  ^ ":"
+  ^ string_of_int record.tm_sec
+
+let make_command_string () =
+  let time =
+    I.string A.(fg lightyellow) (Unix.time () |> time_converter)
+  in
+  let small_line =
+    [| 0x2500; 0x2500; 0x2500; 0x3010 |] |> uchars_maker
+  in
+  let middle_line =
+    [| 0x3011; 0x2500; 0x2500; 0x2500 |] |> uchars_maker
+  in
+  I.(small_line <|> time <|> middle_line)
 
 (** This menu message will be printed when the user types 'menu' into
     the terminal*)
@@ -47,6 +72,8 @@ let read_float () =
 let rec ask_for_commands () =
   (* The arguments of ask_for_commands can be edited to support
      history/accumulation *)
+  make_command_string () |> output_image;
+
   print_string "\n > ";
   try
     match read_line () |> parse with
@@ -83,59 +110,59 @@ let rec ask_for_commands () =
           ("\n" ^ (standard_deviation arguments |> Float.to_string));
         ask_for_commands ()
     | LinReg ->
-        print_string "First list:";
-        print_string "> ";
+        print_string " First list:";
+        print_string " > ";
         let input1 = read_line () in
         let list1 = Commands.parse_list input1 in
-        print_string "Second list:";
-        print_string "> ";
+        print_string " Second list:";
+        print_string " > ";
         let input2 = read_line () in
         let list2 = Commands.parse_list input2 in
         let tuple = linear_regression list1 list2 in
         print_endline
-          ("\n In the form y=ax+b, a = "
+          ( "\n In the form y=ax+b, a = "
           ^ string_of_float (fst tuple)
           ^ " and b = "
-          ^ string_of_float (snd tuple));
+          ^ string_of_float (snd tuple) );
         ask_for_commands ()
     | Poly ->
-        print_endline "Function: ";
-        print_string "> ";
+        print_endline " Function: ";
+        print_string " > ";
         let user_input = read_line () in
         let polyFun =
           user_input |> FrontEnd.parse |> FrontEnd.make_polynomial
           |> FrontEnd.get_fun
         in
-        print_endline "Value to evaluate: ";
-        print_string "> ";
+        print_endline " Value to evaluate: ";
+        print_string " > ";
         let value = read_float () in
         print_endline
           ("Answer: " ^ (value |> polyFun |> string_of_float));
         ask_for_commands ()
     | MultiVar -> failwith "not implemented"
     | Derivative ->
-        print_endline "Function to differentiate: ";
-        print_string "> ";
+        print_endline " Function to differentiate: ";
+        print_string " > ";
         let user_input = read_line () in
         let polyFunDerivative =
           user_input |> FrontEnd.parse |> FrontEnd.make_derivative
           |> FrontEnd.get_fun
         in
-        print_endline "Value to evaluate: ";
-        print_string "> ";
+        print_endline " Value to evaluate: ";
+        print_string " > ";
         let value = read_float () in
         print_endline
           ("Answer: " ^ (value |> polyFunDerivative |> string_of_float));
         ask_for_commands ()
     | Sigma ->
-        print_endline "First: ";
-        print_string "> ";
+        print_endline " First: ";
+        print_string " > ";
         let a = read_float () in
-        print_endline "Second: ";
-        print_string "> ";
+        print_endline " Second: ";
+        print_string " > ";
         let b = read_float () in
-        print_endline "Function: ";
-        print_string "> ";
+        print_endline " Function: ";
+        print_string " > ";
         let user_input = read_line () in
         let polyFun =
           user_input |> FrontEnd.parse |> FrontEnd.make_polynomial
@@ -193,32 +220,50 @@ let start_calc x =
   (* Fill in this function later; its intention is to be used as an
      intermediate loading screen when more complex operations are added. *)
   failwith "unimplemented"
-  let square = "\xe2\x96\xaa"
 
-  let rec sierp n =
-    if n > 1 then
-      let ss = sierp (pred n) in I.(ss <-> (ss <|> ss))
-    else I.(string A.(fg magenta) square |> hpad 1 0)
+(* let square = "\xe2\x96\xaa"
 
-    let rad n color =
-      let a1 = A.fg color in
-      let a2 = A.(st blink ++ a1) in
-      I.((string a2 "Rad" |> hpad n 0) <->
-         (string a1 "(⌐■_■)" |> hpad (n + 7) 0))
-    
-    let colors = A.[red; green; yellow; blue; magenta; cyan]
+   let rec sierp n = if n > 1 then let ss = sierp (pred n) in I.(ss <->
+   (ss <|> ss)) else I.(string A.(fg magenta) square |> hpad 1 0)
+
+   let rad n color = let a1 = A.fg color in let a2 = A.(st blink ++ a1)
+   in I.( string a2 "Rad" |> hpad n 0 <-> (string a1 "(⌐■_■)" |> hpad (n
+   + 7) 0)) *)
+
+(* let colors = A.[ red; green; yellow; blue; magenta; cyan ] *)
+
 (** [main ()] prompts for the game to play, then starts it. *)
+
 let main () =
-  ANSITerminal.print_string
-    [ ANSITerminal.green; ANSITerminal.Bold ]
-    "\n\n\
-    \ Welcome to the ClammyAlpha Calculator. This calculator was \
-     developed by the esteemed AHA corporation. The main developers \
-     are AWang, HuyBear and Kangaroo. Please enjoy. \n";
-   Notty_unix.output_image (sierp 4);
-   Notty_unix.output_image (colors |> List.mapi I.(fun i c -> rad i c |> pad ~t:i ~l:(2 * i))
-   |> I.zcat);
-  print_endline "\n\n Type any key to start, or type Exit to quit.";
+  (* ANSITerminal.print_string [ ANSITerminal.green; ANSITerminal.Bold ]
+     "\n\n\ \ Welcome to the ClammyAlpha Calculator. This calculator was
+     \ developed by the esteemed AHA corporation. The main developers \
+     are AWang, HuyBear and Kangaroo. Please enjoy. \n"; *)
+  let long_line = Array.make 100 0x2500 in
+  uchars_maker long_line |> Notty_unix.output_image;
+
+  let description =
+    I.string
+      A.(fg lightyellow ++ bg lightblack ++ st underline ++ st bold)
+      "Welcome to the ClammyAlpha Calculator!"
+  in
+  I.(pad ~l:1 ~t:2 ~b:2 description) |> Notty_unix.output_image;
+  let company =
+    I.string
+      A.(fg white)
+      "Developed by the eestemed AHA Corporation with"
+  in
+  I.(pad ~l:1 ~b:2 company) |> Notty_unix.output_image;
+  let devs =
+    I.string
+      A.(fg white)
+      "Chief Developers AWang, HuyBear, and Kangaroo."
+  in
+  I.(pad ~l:1 ~b:2 devs) |> Notty_unix.output_image;
+  I.uchars A.(fg lightred) (Array.map Uchar.of_int long_line)
+  |> Notty_unix.eol |> Notty_unix.output_image;
+  print_endline "\n\n Type any key to start, or type Exit to quit.\n";
+  make_command_string () |> output_image;
   print_string "\n\n > ";
   match read_line () with
   | "Exit" ->
