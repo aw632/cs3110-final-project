716796523ea6992ecfdafdafc13f397f002080a9
aw632 <aw632@cornell.edu>
2021-05-20 15:01:29 -0400

    added test plan, added test cases for derivative

--- test.ml
+++ test.ml
@@ -4,6 +4,36 @@ open Commands
 open EuclideanAlg
 open FrontEnd
 
+(** ====================================
+
+    TEST SUITE AND TESTING METHODOLOGY
+
+    ====================================
+
+    We tested all user-accessible operations in our calculator. This
+    means we excluded explicitly testing any helper functions, therefore
+    we use BlackBox testing. Test cases were hence developed with this
+    mind, and were mostly developed manually. Randomized test cases were
+    not used.
+
+    The OUnit below tests the following modules directly or indirectly:
+    BasicOp Commands EuclideanAlg Dual Derviative StatOp
+
+    The OUnit does not test the following modules: Author Main Help
+
+    The following modules were tested manually: Main
+
+    We manually tested the REPL, as it is quite difficult to test it
+    with OUnit. However, the Main module (which is the REPL) is mainly a
+    hub which refers to other modules with logic within them. Therefore,
+    as long as we test the correctness of the operations accessible from
+    Main, we can demonstrate the correctness of Main.
+
+    We did not explicitly test anything having to do with Dual Numbers,
+    as the facts and properties about Dual Numbers are mathematically
+    proven to be correct. We only tested that the AST was able to parse
+    a derivative and evaluate it - using dual numbers - successfully. *)
+
 let basic_op_test name expected_output f input_list =
   name >:: fun info ->
   assert_equal expected_output (f input_list) ~printer:string_of_float
@@ -225,6 +255,39 @@ let command_tests =
       "fastexp 21 2 1101";
   ]
 
+let deriv_test name expected_output input value =
+  name >:: fun info ->
+  let polyFunDerivative =
+    input |> FrontEnd.parse |> FrontEnd.make_derivative
+    |> FrontEnd.get_fun
+  in
+  assert_equal expected_output
+    (value |> polyFunDerivative |> string_of_float)
+
+let derivative_tests =
+  [
+    deriv_test "Derivative of x^2 at 1.5 is 3" "3." "x^2" 1.5;
+    deriv_test "Derivative of x^2 at 0 is 0" "0." "x^2" 0.;
+    deriv_test "Derivative of a multiple term polynomial" "70."
+      "x^2 + 66x + 3" 2.;
+    deriv_test "derivative of constant" "0." "396644" 55.;
+    deriv_test "derivative of single variable" "396644." "396644x"
+      1234444.;
+    deriv_test "derivative of cubic" "53." "x^3 + 5x + 9" 4.;
+    deriv_test "derivative of fourthic" "131811524."
+      "555x^4 + 123344x + 1245559980" 39.;
+    deriv_test "derivative of quintic" "23134410." "2x^5 + 0x^3 + 2" 39.;
+    deriv_test "derivative of a lot of zeroes" "0." "0x^3+0x^6" 3.;
+    deriv_test "derivative of x^2+5x+193" "9." "x^2+5x+193" 2.;
+    deriv_test "derivative of 6x+5" "6." "6x+5" 5.;
+    deriv_test "derivative of 9x+2" "9." "9x+2" 0.;
+    deriv_test "derivative of 111x^5" "555." "111x^5" 1.;
+    deriv_test "derivative of big coeffcient" "499990." "99998x^5" 1.;
+    deriv_test "derivative of 8th degree" "8." "x^8" 1.;
+    deriv_test "derivative of 5th  + 9th degree" "1232." "x^5+0.5x^9" 2.;
+    deriv_test "derivative of simple function" "900." "900x" 3.;
+  ]
+
 let suite =
   "test suite for operations"
   >::: List.flatten
@@ -234,6 +297,7 @@ let suite =
            function_parse_tests;
            parser_ast_tests;
            binop_parse_tests;
+           derivative_tests;
          ]
 
 let _ = run_test_tt_main suite
