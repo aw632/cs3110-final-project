e3f3f6a45b7e1810f4948ff5d6872531e9ddd820
Andrew Lou <ayl72@cornell.edu>
2021-05-22 20:17:59 -0400

    Added some trig functions

---
+++ trig.ml
@@ -0,0 +1,16 @@
+exception TriangleDNE
+
+let sin arg = sin (arg *. Float.pi /. 180.)
+
+let tan arg = tan (arg *. Float.pi /. 180.)
+
+let cos arg = cos (arg *. Float.pi /. 180.)
+
+let pythag part length1 length2 =
+  match part with
+  | "hypotenuse" -> sqrt ((length1 *. length1) +. (length2 *. length2))
+  | "leg" ->
+      if (length1 *. length1) -. (length2 *. length2) > 0. then
+        sqrt ((length1 *. length1) +. (length2 *. length2))
+      else raise TriangleDNE
+  | _ -> failwith "Will not be reached"
