a223f71765e465bb76b594d28beb21c9b2e8f889
Andrew Lou <ayl72@cornell.edu>
2021-05-23 12:00:36 -0400

    Editing menu message

---
+++ helpMessage.ml
@@ -0,0 +1,29 @@
+let menu_msg =
+  "\n\
+  \ \n\
+  \ Please enter an operation, followed by a space, followed by \n\
+  \ the numbers you want to operate on.\n\n\
+  \   Functions available:\n\
+  \   Add (takes in multiple inputs, returns float) \n\
+  \   Subtract (takes in multiple inputs, returns float) \n\
+  \   Divide (takes in multiple inputs, returns float)\n\
+  \   Multiply (takes in multiple inputs, returns float)\n\
+  \   Factorial (takes in one input, returns integer)\n\
+  \   FastExp (takes in three inputs, returns integer)\n\
+  \   Sin (takes in one input, returns float)\n\
+  \   Cos (takes in one input, returns float)\n\
+  \   Tan (takes in one input, returns float)\n\
+  \   Pythag (takes in two sides, returns third side as float) \n\
+  \   Mean (takes in multiple input, returns float)\n\
+  \   Median (takes in multiple input, returns float)\n\
+  \   StdDev (takes in multiple input, returns float)\n\
+  \   LinReg (takes in two lists, returns linear regression)\n\
+  \   Poly (takes in a function and a value to evaluate the function)\n\
+  \   Sigma (evaluates the sigma from the first number (floor) to the \n\
+  \        second number (ceiling) using the user-inputted polynomial)\n\
+  \   Derivative (takes in a function and a value to evaluate the \n\
+  \        derivative at the value)\n\n\
+  \ Enter Exit at any time to exit from the program\n\
+  \ If you want more information, use command 'help' followed \n\
+  \ by the function you would like to learn more about. \n\
+  \ "
